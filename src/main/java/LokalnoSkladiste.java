beca628a1935c1b1907404553a7b23530412a0be
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-10-26 13:11:40 +0200

    init

---
+++ src/main/java/LokalnoSkladiste.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class LokalnoSkladiste{
+
+    public LokalnoSkladiste() {
+    }
+
+
+    //    public void createRoot(String path,String name){
+//       path = path + '\'' + name;
+//       File root = new File(path);
+//       boolean b = root.mkdir();
+//       if(b){
+//           System.out.println("root is created successfully");
+//       }else
+//           System.out.println("Error!");
+//    }
+
+
+    //public static void main(String[] args) {
+
+        //createRoot("C:\\Users\\Milos\\OneDrive\\Desktop","Proba");
+        /*System.out.println("Enter the path where you want to create a folder: ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.next();
+        //Using Scanner class to get the folder name from the user
+        System.out.println("Enter the name of the desired a directory: ");
+        path = path+sc.next();
+        //Instantiate the File class
+        File f1 = new File(path);
+        //Creating a folder using mkdir() method
+        boolean bool = f1.mkdir();
+        if(bool){
+            System.out.println("Folder is created successfully");
+        }else{
+            System.out.println("Error Found!");
+        }*/
+
+    //}
+    public void createRoot(String path,String name){
+        path = path + '\\' + name;
+        File root = new File(path);
+        boolean b = root.mkdir();
+        if(b){
+            System.out.println("root is created successfully");
+        }else
+            System.out.println("Error!");
+    }
+    public void checkIfRootExists(String path){
+        File f = new File(path);
+        if(f.exists()){
+            System.out.println("Root is already created");
+        }else{
+            createRoot(path,"newRoot");
+        }
+    }
+}
---
+++ src/main/java/ProbnaKlasa.java
@@ -0,0 +1,9 @@
+public class ProbnaKlasa {
+
+
+    public static void main(String[] args) {
+        LokalnoSkladiste ls = new LokalnoSkladiste();
+        //ls.createRoot("C:\\Users\\Milos\\OneDrive\\Desktop", "root1");
+        ls.checkIfRootExists("C:\\Users\\Milos\\OneDrive\\Desktop\\root1");
+    }
+}
